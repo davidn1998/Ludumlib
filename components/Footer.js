e82e94837026ebe3927223ea9dc77949c87a4590
David Nwachukwu <davidnw1998@gmail.com>
2021-02-22 23:34:45 +0000

    edit footer styles

--- components/Footer.js
+++ components/Footer.js
@@ -5,20 +5,24 @@ import styles from "../components/Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>
-        Copyright © 2020 Ludumlib, Inc. All rights reserved. |
-        <Link href="/terms">
-          <a> Terms and Conditions</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/privacy-policy">
-          <a> Privacy Policy</a>
-        </Link>{" "}
-        | Game data from{" "}
-        <a href="https://rawg.io/" target="_blank" rel="noopener noreferrer">
-          RAWG.
-        </a>
-      </p>
+      <p>Copyright © 2020 Ludumlib</p>
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <a href="https://rawg.io/" target="_blank" rel="noopener noreferrer">
+            Game data from RAWG
+          </a>
+        </div>
+        <div className={styles.link}>
+          <Link href="/terms">
+            <a> Terms and Conditions</a>
+          </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="/privacy-policy">
+            <a> Privacy Policy</a>
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
